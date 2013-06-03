d3f4161d9f09b2597febda971b5c981ec9740cbf
Thijs de Zoete <thijsdezoete@gmail.com>
2013-06-03 18:26:37 +0200

    Factory pattern for Request and Database objects

---
+++ src/factory.py
@@ -0,0 +1,21 @@
+class KolFactory(object):
+    def __new__(klass, name, *args, **kwargs):
+        _base = 'kol'
+        _module_name = 'request'
+
+        if 'Database' in name:
+            _module_name = 'database'
+
+        _full_mod = '.'.join([_base, _module_name, name])
+        print _full_mod
+        _func_name = [name] if args or kwargs else name
+
+        _my_mod = __import__(_full_mod, globals(), locals(), _func_name, -1)
+        if not args and not kwargs:
+            return _my_mod
+        else:
+            return getattr(_my_mod, name)(*args, **kwargs)
+
+    def __init__(self, *args, **kwargs):
+        """ KolFactory by Thijs de Zoete """
+        pass
